b555dbf41239f30c86d3e99fae316d9dda9d126c
ashikur rahman <nillakash4097@gmail.com>
2024-10-09 17:02:51 +0600

    CreateProjectAndClassLibraries

---
+++ EducationManagementSystem/EducationManagementSystem.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EducationManagementSystem.Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplicationDependency(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ EducationManagementSystem/EducationManagementSystem.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EducationManagementSystem.Infrastructure;
+
+public static class InfrastructureDependencyInjection
+{
+    public static IServiceCollection AddInfrastructureDependency(this IServiceCollection services,IConfiguration configuration)
+    {
+        return services;
+    }
+}
